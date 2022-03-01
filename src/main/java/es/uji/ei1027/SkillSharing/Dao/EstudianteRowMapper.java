4763260e025238119772e50f3ac58ad170e5888b
Jorge <al342510@uji.es>
2022-03-01 16:01:44 +0100

    Clase EstudianteRowMapper.

--- src/main/java/es/uji/ei1027/SkillSharing/Dao/EstudianteRowMapper.java
+++ src/main/java/es/uji/ei1027/SkillSharing/Dao/EstudianteRowMapper.java
@@ -1,4 +1,20 @@
 package es.uji.ei1027.SkillSharing.Dao;
 
-public class EstudianteRowMapper {
+
+import es.uji.ei1027.SkillSharing.ObjetosDeDominio.Estudiante;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public final class EstudianteRowMapper implements RowMapper<Estudiante> {
+    public Estudiante mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Estudiante estudiante = new Estudiante();
+        estudiante.setDni_alumno(rs.getString("dni_alumno"));
+        estudiante.setNombre_completo(rs.getString("nombre_completo"));
+        estudiante.setEmail(rs.getString("email"));
+        estudiante.setPassword(rs.getString("password"));
+        estudiante.setBaneado(rs.getBoolean("baneado"));
+        return estudiante;
+    }
 }
