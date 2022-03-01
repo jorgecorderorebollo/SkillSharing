0807b4663fd47c992dd4ab077a5a18a864004383
Jorge <al342510@uji.es>
2022-03-01 16:10:56 +0100

    Clase ColaboracionRowMapper.

--- src/main/java/es/uji/ei1027/SkillSharing/Dao/ColaboracionRowMapper.java
+++ src/main/java/es/uji/ei1027/SkillSharing/Dao/ColaboracionRowMapper.java
@@ -1,4 +1,22 @@
 package es.uji.ei1027.SkillSharing.Dao;
 
-public class ColaboracionRowMapper {
+
+import es.uji.ei1027.SkillSharing.ObjetosDeDominio.Colaboracion;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public final class ColaboracionRowMapper implements RowMapper<Colaboracion> {
+    public Colaboracion mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Colaboracion colaboracion = new Colaboracion();
+        colaboracion.setCodigo_colaboracion(rs.getString("codigo_colaboracion"));
+        colaboracion.setStartdate(rs.getDate("fecha_inicio"));
+        colaboracion.setEnddate(rs.getDate("fecha_fin"));
+        colaboracion.setHoras(rs.getFloat("horas"));
+        colaboracion.setEvaluacion(rs.getString("evaluacion"));
+        colaboracion.setCodigo_oferta(rs.getString("codigo_oferta"));
+        colaboracion.setCodigo_solicitud(rs.getString("codigo_solicitud"));
+        return colaboracion;
+    }
 }
