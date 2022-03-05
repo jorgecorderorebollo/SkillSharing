ef21679e422325b6538a99f8f8701b3fc62ac96b
Jorge <al342510@uji.es>
2022-03-05 06:05:09 +0100

    Modificación.

--- src/main/java/es/uji/ei1027/SkillSharing/SkillSharingApplication.java
+++ src/main/java/es/uji/ei1027/SkillSharing/SkillSharingApplication.java
@@ -1,21 +1,14 @@
 package es.uji.ei1027.SkillSharing;
 
 import java.util.logging.Logger;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class SkillSharingApplication implements CommandLineRunner {
+public class SkillSharingApplication{
 	private static final Logger log = Logger.getLogger(SkillSharingApplication .class.getName());
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(SkillSharingApplication.class).run(args);
-		//SpringApplication.run(SkillSharingApplication.class, args);
 	}
-	public void run(String... strings) throws Exception {
-		log.info("Ací va el meu codi");
-	}
-
-
 }
