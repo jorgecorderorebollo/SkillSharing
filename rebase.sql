8285d3b323ad265cfa04bdda11fa8ebdbc771dd9
Dan <36074341+DanPeia96@users.noreply.github.com>
2022-05-07 02:40:50 +0200

    Update rebase.sql

--- rebase.sql
+++ rebase.sql
@@ -6,3 +6,24 @@ DROP TABLE estudiante;
 DROP TABLE promotor;
 DROP TABLE habilidad;
 DROP TABLE usuario;
+
+
+CREATE TABLE Usuario(
+  id_usuario        VARCHAR(9) NOT NULL,
+  nombre_completo   VARCHAR(50) NOT NULL,
+  email             VARCHAR(50) NOT NULL,
+  password          VARCHAR(50) NOT NULL,
+  activado          BOOLEAN DEFAULT TRUE,
+  admin             BOOLEAN DEFAULT FALSE,              
+  saldo_horas       FLOAT NOT NULL,
+  
+  CONSTRAINT pk_usuario PRIMARY KEY(id_usuario),
+  CONSTRAINT ri_saldo_disponible CHECK (saldo_horas>0)
+  );
+  
+  CREATE TABLE Habilidad(
+    nombre        VARCHAR(50),
+    nivel         VARCHAR(50),
+    descripcion   VARCHAR(200),
+    CONSTRAINT
+  );
