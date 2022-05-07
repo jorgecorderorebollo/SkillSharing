efa53e33d8597ccc3fc86a3acad2f2d358b5b7b1
Dan <36074341+DanPeia96@users.noreply.github.com>
2022-05-07 21:38:16 +0200

    Update rebase.sql

--- rebase.sql
+++ rebase.sql
@@ -8,7 +8,7 @@ DROP TABLE habilidad;
 DROP TABLE usuario;
 
 
-CREATE TABLE Usuario(
+CREATE TABLE usuario(
   id_usuario        VARCHAR(9) NOT NULL,
   nombre_completo   VARCHAR(50) NOT NULL,
   email             VARCHAR(50) NOT NULL,
@@ -21,9 +21,28 @@ CREATE TABLE Usuario(
   CONSTRAINT ri_saldo_disponible CHECK (saldo_horas>0)
   );
   
-  CREATE TABLE Habilidad(
-    nombre        VARCHAR(50),
-    nivel         VARCHAR(50),
+  CREATE TABLE habilidad(
+    nombre        VARCHAR(20),
+    nivel         VARCHAR(20),
     descripcion   VARCHAR(200),
-    CONSTRAINT
+    CONSTRAINT pk_codigo_habilidad PRIMARY KEY(nombre, nivel)
   );
+
+CREATE TABLE oferta(
+    codigo_oferta   VARCHAR(6) NOT NULL,
+    fecha_inicio    DATA,
+    fecha_fin       DATE,
+    descripcion     VARCHAR(500),
+    id_usuario      VARCHAR(9),
+    nombre_habilidad  VARCHAR(20),
+    nivel_habilidad   VARCHAR(20),
+    CONSTRAINT pk_codigo_oferta PRIMARY KEY(codigo_oferta),
+    CONSTRAINT ca_id_usuario FOREIGN KEY(id_usuario)
+    REFERENCES usuariO ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT ca_habilidad FOREIGN KEY(nombre, nivel)
+    REFERENCES (habilidad) (nombre, nivel) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONTRAINT ri_fini_ffin CHECK (fecha_fin>fecha_inicio)
+  );
+  
+  CREATE TABLE solicitud(
+  
